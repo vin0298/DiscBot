50039603ce4943458a02eee7bfefe369556cb77a
Vincent Surya Bensawan <bensawan0298@gmail.com>
2019-03-30 17:58:42 -0700

    Added a simple bot command and added bot to server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+// import discord library
+const Discord = require('discord.js');
+// creates the client
+const bot = new Discord.Client();
+// 66156848 - permission
+
+// simple bot command
+client.on('message', (message) => {
+    if (message.content == 'test') {
+        message.reply('ok');
+    }
+});
+
+bot.login('NTYxNzA5MjM2OTI1NzU5NTA5.XKALJQ.jIP5zCSIMczpiwObN2AVApCduBk');
