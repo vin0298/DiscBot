6a034c98bfb50ecf79a5a72ca703d67ce2790209
Vincent Surya Bensawan <bensawan0298@gmail.com>
2019-04-02 14:55:27 -0700

    Implemented single url music playing

--- commands/play.js
+++ commands/play.js
@@ -10,8 +10,33 @@ module.exports = {
         if (!message.member.voiceChannel) {
             return message.channel.send(`You need to be in a voice channel`);
         }
-        // Check link
+
+        if (args.length > 1) {
+            return message.reply(`Please just supply one argument or URL`);
+        }
+
+        // Parse the link
+        url = args[0];
+        message.channel.send(url); // URL Parsing test
+
+        // Check if link is valid
+        ytdl.getInfo(url, function(err, info) {
+            if (err) {
+                return message.reply(`Invalid URL. Please retry with a valid URL`);
+            }
+        });
+
         const voiceChannel = message.member.voiceChannel;
-        voiceChannel.join();
+        voiceChannel.join()
+            .then(connection => {
+                message.channel.send("Successfully joined voice channel");
+
+                const musicStream = ytdl(url, {filter: 'audioonly'});
+                const dispatcher = connection.playStream(musicStream);
+                dispatcher.on("end", end => {
+                    message.channel.send('Music ended, left voice channel');
+                    voiceChannel.leave();
+                })
+            })
 	},
 };
