e28ca653c8176ff8e89c9c94cbd10560c44a0006
Vincent Surya Bensawan <bensawan0298@gmail.com>
2019-04-03 16:15:32 -0700

    Fixed a bug on stop function

--- commands/stop.js
+++ commands/stop.js
@@ -3,10 +3,14 @@ const {prefix} = require('../config.json');
 module.exports = {
 	name: 'stop',
     description: 'Command to stop playing music',
+
 	execute(message) {
         if (message.guild.voiceConnection) {
             // Empty the queue
             let emptyQueue = [];
+            if (servers[message.guild.id].dispatcher) {
+                servers[message.guild.id].dispatcher.destroy();
+            }
             servers[message.guild.id] = emptyQueue;
         }
 
