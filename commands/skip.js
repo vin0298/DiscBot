d5b76cb2b6d206a3dae830643d3ce0631ac8d109
Vincent Surya Bensawan <bensawan0298@gmail.com>
2019-04-03 16:03:11 -0700

    Implemented skip function

---
+++ commands/skip.js
@@ -0,0 +1,23 @@
+const {prefix} = require('../config.json');
+
+module.exports = {
+	name: 'skip',
+    description: 'Command to skip and start playing the next music',
+	execute(message) {
+        if (!message.member.voiceChannel) {
+            return message.channel.send(`You need to be in a voice channel to use this command`);
+        }
+
+        if (message.guild.voiceConnection) {
+            // Get the queue for the current server
+            var curServer = servers[message.guild.id];
+            
+            if (curServer.dispatcher) {
+               return curServer.dispatcher.destroy();
+            }
+        
+        } else {
+            return message.channel.send('Bot not in a voice channel - nothing to skip');
+        }
+	},
+};
--- commands/stop.js
+++ commands/stop.js
@@ -3,12 +3,11 @@ const {prefix} = require('../config.json');
 module.exports = {
 	name: 'stop',
     description: 'Command to stop playing music',
-    usage: `\`${prefix}stop\``,
 	execute(message) {
         if (message.guild.voiceConnection) {
             // Empty the queue
-            let em = [];
-            servers[message.guild.id] = em;
+            let emptyQueue = [];
+            servers[message.guild.id] = emptyQueue;
         }
 
         message.member.voiceChannel.leave();
